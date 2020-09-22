8cf031f59a8ec760132a9e6df662c3f99af510a3
tommywalkie <tommywalkie@gmail.com>
2020-09-22 23:51:45 +0200

    v0.0.10 -> improved formatting process

--- src/element.ts
+++ src/element.ts
@@ -1,6 +1,6 @@
 import {compile, serialize, stringify} from 'stylis'
-import {MagnosticStyle} from './css'
-import {MagnosticKeyframes} from './keyframes'
+import type {MagnosticStyle} from './css'
+import type {MagnosticKeyframes} from './keyframes'
 import {customAlphabet} from 'nanoid'
 
 const uniqueId: () => string = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10)
@@ -19,7 +19,7 @@ export enum MagnosticElementTypePrefix {
 	KEYFRAMES = 'anim'
 }
 
-export const format = (rendered: string, stringProps: string[]): string => {
+const format = (rendered: string, stringProps: string[]): string => {
 	let index = -1
 	return rendered.replace(/:,[^;]*;/gm, occurance => {
 		index += 1
@@ -49,12 +49,14 @@ export class MagnosticElement {
 		this.template = template
 		const stringProps: string[] = []
 		const renderedProps: Array<string | undefined> = props.map((element: MagnosticProp) => {
-			if ((element as MagnosticStyle).type && (element as MagnosticStyle).type === MagnosticElementTypeAlias.STYLE)
-				return `${(element as MagnosticElement).template}\n`
-			if ((element as MagnosticKeyframes).type && (element as MagnosticKeyframes).type === MagnosticElementTypeAlias.KEYFRAMES)
-				stringProps.push(`__k__${element}__k__`)
-			else
-				stringProps.push(String(element))
+            switch((element as MagnosticElement).type) {
+                case MagnosticElementTypeAlias.STYLE:
+                    return `${(element as MagnosticStyle).template}\n`
+                case MagnosticElementTypeAlias.KEYFRAMES:
+                    stringProps.push(`__k__${(element as MagnosticKeyframes)}__k__`)
+                default:
+                    stringProps.push(String(element))
+            }
 			return undefined
 		})
 		const rendered = serialize(compile(`${selector}${this.className} { ${renderedProps} ${template} }`), stringify)
